c0f479df16e24e5cc5926c063e656baa04981a4d
Ajesh <ajesh@prixa.org>
2018-10-02 21:38:07 +0545

    Github link updated

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
 	description="Import/Export feature for django models",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
-	url="https://github.com/pypa/sampleproject",
+	url="https://github.com/aj3sh/model_import_export",
 	packages=setuptools.find_packages(),
 	classifiers=[
 
