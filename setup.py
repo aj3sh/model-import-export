2d710aac40588f460b6d2633c918d8ff6bef0a15
Ajesh <ajesh@prixa.org>
2018-10-05 13:04:02 +0545

    setup updated (requirements & publish options added)

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+import os
+import sys
 import setuptools
 
 with open("README.md", "r") as fh:
 	long_description = fh.read()
 
+
+if sys.argv[-1] == 'publish':
+	if os.system("pip freeze | grep twine"):
+		print("twine not installed.\nUse `pip install twine`.\nExiting.")
+		sys.exit()
+	os.system("python3 setup.py sdist bdist_wheel")
+	os.system("twine upload dist/*")
+	sys.exit()
+
+
 setuptools.setup(
 	name="model_import_export",
-	version="0.0.2",
+	version="0.0.3",
 	author="Ajesh Sen Thapa",
 	author_email="aj3sshh@gmail.com",
 	description="Import/Export feature for django models",
@@ -13,6 +25,15 @@ setuptools.setup(
 	long_description_content_type="text/markdown",
 	url="https://github.com/aj3sh/model_import_export",
 	packages=setuptools.find_packages(),
+	install_requires=[
+
+		'numpy==1.15.2',
+		'openpyxl==2.5.8',
+		'pandas==0.23.4',
+		'xlrd==1.1.0',
+		'xlwt==1.3.0',
+	
+	],
 	classifiers=[
 
 		'Environment :: Web Environment',
